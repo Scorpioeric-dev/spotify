589049a2633df452d86da1784992253cd08ff969
Scorpioeric-dev <ericscorpio1@gmail.com>
2021-02-23 20:38:41 -0800

    authentication-token

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import React ,{useEffect} from 'react'
 import './App.css';
 import { Login } from './Components/Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  
+  useEffect(() => {
+    
+    const token = getTokenFromUrl()
+    console.log('I have a token >>>',token)
+  }, [])
   return (
     <div className="app">
      
