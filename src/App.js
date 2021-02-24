c4dd73b17a29dc5b0134a34ec75cc6eca62eae1f
Scorpioeric-dev <ericscorpio1@gmail.com>
2021-02-24 07:34:01 -0800

    spotify

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React ,{useEffect} from 'react'
-import './App.css';
-import { Login } from './Components/Login';
-import { getTokenFromUrl } from './spotify';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Login } from "./Components/Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import { Player } from "./Components/Player";
+
+//Wrapping spotify by creating a new API
+const spotify = new SpotifyWebApi();
 
 function App() {
-  
+  const [token, setToken] = useState(null);
   useEffect(() => {
-    
-    const token = getTokenFromUrl()
-    console.log('I have a token >>>',token)
-  }, [])
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      //Opening channel between spotify and this React app
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("person", user);
+      });
+    }
+
+    console.log("I have a token >>>", token);
+  }, []);
+
   return (
     <div className="app">
-     
-     {/*Spotify Logo */}
-     
-     {/* Login with spotify Button */}
-     <Login/>
+      {token ? <Player/> : <Login />}
+      {/*Spotify Logo */}
+
+      {/* Login with spotify Button */}
     </div>
   );
 }
---
+++ src/Components/Player.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Style/Player.css'
+
+
+export const Player = () => {
+    return (
+        <div className="player">
+        <div className="player_body">
+        {/*SideBar */}
+         {/*Body */}
+        <h1>Welcome to Spotify</h1> 
+        </div>
+        {/* Footer */}
+        </div>
+    )
+}
